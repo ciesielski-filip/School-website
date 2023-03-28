ce4080595278aeb17e42da3a4c3e17d7bb44450b
ciesielski-filip <filipciesielski2005@gmail.com>
2023-03-28 19:30:45 +0200

    .

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const checkbox_t = document.querySelector(".checkbox_t");
 const darkModeEnabled = localStorage.getItem("darkModeEnabled") === "true";
 
-if (darkModeEnabled) {
-  document.documentElement.classList.add("dark-mode");
-  checkbox_t.checked = true;
+if (darkModeEnabled === true) {
+    document.documentElement.classList.add("dark-mode");
+    checkbox_t.checked = true;
 }
 
 checkbox_t.addEventListener('change', function() {
-  document.documentElement.classList.toggle("dark-mode");
-  localStorage.setItem("darkModeEnabled", checkbox_t.checked);
+    document.documentElement.classList.toggle("dark-mode");
+    localStorage.setItem("darkModeEnabled", checkbox_t.checked);
 });
