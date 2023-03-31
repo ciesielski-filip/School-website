368e232d5841bd5a38ab857eca21b000ca90bb69
ciesielski-filip <filipciesielski2005@gmail.com>
2023-03-31 21:06:34 +0200

    .

---
+++ js/o_nas.js
@@ -0,0 +1,8 @@
+// document.addEventListener("DOMContentLoaded", function(event) {
+//     let blocks = document.querySelectorAll(".block")
+//     blocks.forEach(function(block, delay) {
+//         block.classList.add("anim")
+//         block.style.animationDelay = (delay * 0.5) + "s"
+//         block.style.opacity = 1
+//     })
+// })
