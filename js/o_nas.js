fb2f5abb56b57a725f620b11c855030668590a9e
ciesielski-filip <filipciesielski2005@gmail.com>
2023-04-01 14:03:26 +0200

    .

--- js/o_nas.js
+++ js/o_nas.js
@@ -9,12 +9,19 @@
 
 
 let block1 = document.querySelector(".block1")
-let content1 = document.querySelector(".content1")
-
+let content1 = document.querySelector(".content:nth-child(1)")
 block1.addEventListener("click", function() {
     content1.classList.add("show-content")
 })
-
 content1.addEventListener("click", function() {
     content1.classList.remove("show-content")
+})
+
+let block2 = document.querySelector(".block2")
+let content2 = document.querySelector(".content:nth-child(2)")
+block2.addEventListener("click", function() {
+    content2.classList.add("show-content")
+})
+content2.addEventListener("click", function() {
+    content2.classList.remove("show-content")
 })
