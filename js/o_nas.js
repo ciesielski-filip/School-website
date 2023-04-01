aea422a2179c93d53c02b4bfaf196848f832b0e1
ciesielski-filip <filipciesielski2005@gmail.com>
2023-04-01 14:17:26 +0200

    .

--- js/o_nas.js
+++ js/o_nas.js
@@ -15,8 +15,16 @@ function toggleContent(block, content) {
         content.classList.remove("show-content")
     })
 }
-toggleContent(document.querySelector(".block1"), document.querySelector(".content1"))
-toggleContent(document.querySelector(".block2"), document.querySelector(".content2"))
-toggleContent(document.querySelector(".block3"), document.querySelector(".content3"))
-toggleContent(document.querySelector(".block4"), document.querySelector(".content4"))
-toggleContent(document.querySelector(".block5"), document.querySelector(".content5"))
+
+const blocks = document.querySelectorAll(".block")
+const contents = document.querySelectorAll(".content")
+
+blocks.forEach((block, index) => {
+    toggleContent(block, contents[index])
+})
+
+// toggleContent(document.querySelector(".block1"), document.querySelector(".content1"))
+// toggleContent(document.querySelector(".block2"), document.querySelector(".content2"))
+// toggleContent(document.querySelector(".block3"), document.querySelector(".content3"))
+// toggleContent(document.querySelector(".block4"), document.querySelector(".content4"))
+// toggleContent(document.querySelector(".block5"), document.querySelector(".content5"))
