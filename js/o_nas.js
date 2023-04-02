30113f6e5e9fb4b6a0952715b726bad52515055c
ciesielski-filip <filipciesielski2005@gmail.com>
2023-04-02 11:51:25 +0200

    .

--- js/o_nas.js
+++ js/o_nas.js
@@ -7,24 +7,17 @@
 //     })
 // })
 
-function toggleContent(block, content) {
+function toggleContent(block, content, close) {
     block.addEventListener("click", function() {
         content.classList.add("show-content")
     })
-    content.addEventListener("click", function() {
+    close.addEventListener("click", function() {
         content.classList.remove("show-content")
     })
 }
+toggleContent(document.querySelector(".block1"), document.querySelector(".content1"), document.querySelector(".close1"))
+toggleContent(document.querySelector(".block2"), document.querySelector(".content2"), document.querySelector(".close2"))
+toggleContent(document.querySelector(".block3"), document.querySelector(".content3"), document.querySelector(".close3"))
+toggleContent(document.querySelector(".block4"), document.querySelector(".content4"), document.querySelector(".close4"))
+toggleContent(document.querySelector(".block5"), document.querySelector(".content5"), document.querySelector(".close5"))
 
-const blocks = document.querySelectorAll(".block")
-const contents = document.querySelectorAll(".content")
-
-blocks.forEach((block, index) => {
-    toggleContent(block, contents[index])
-})
-
-// toggleContent(document.querySelector(".block1"), document.querySelector(".content1"))
-// toggleContent(document.querySelector(".block2"), document.querySelector(".content2"))
-// toggleContent(document.querySelector(".block3"), document.querySelector(".content3"))
-// toggleContent(document.querySelector(".block4"), document.querySelector(".content4"))
-// toggleContent(document.querySelector(".block5"), document.querySelector(".content5"))
