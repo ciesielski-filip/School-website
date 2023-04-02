62e45bd50feabf9e6efa8af6d45c92b0e6f7c8b7
ciesielski-filip <filipciesielski2005@gmail.com>
2023-04-02 11:58:01 +0200

    .

--- js/o_nas.js
+++ js/o_nas.js
@@ -20,4 +20,3 @@ toggleContent(document.querySelector(".block2"), document.querySelector(".conten
 toggleContent(document.querySelector(".block3"), document.querySelector(".content3"), document.querySelector(".close3"))
 toggleContent(document.querySelector(".block4"), document.querySelector(".content4"), document.querySelector(".close4"))
 toggleContent(document.querySelector(".block5"), document.querySelector(".content5"), document.querySelector(".close5"))
-
