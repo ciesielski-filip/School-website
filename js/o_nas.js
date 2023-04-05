8da90b0a38f99746eeae8ccf478eb12a5e3b7a46
ciesielski-filip <filipciesielski2005@gmail.com>
2023-04-05 12:54:00 +0200

    .

--- js/o_nas.js
+++ js/o_nas.js
@@ -1,12 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function(event) {
-//     let blocks = document.querySelectorAll(".block")
-//     blocks.forEach(function(block, delay) {
-//         block.classList.add("anim")
-//         block.style.animationDelay = (delay * 0.5) + "s"
-//         block.style.opacity = 1
-//     })
-// })
-
 function toggleContent(block, content, close, blur) {
     block.addEventListener("click", function() {
         content.classList.add("show-content")
