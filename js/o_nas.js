4f8174874aa41f4ee268cdbb2d11c56b9b1dc09e
ciesielski-filip <filipciesielski2005@gmail.com>
2023-04-05 12:29:33 +0200

    .

--- js/o_nas.js
+++ js/o_nas.js
@@ -7,19 +7,22 @@
 //     })
 // })
 
-function toggleContent(block, content, close) {
+function toggleContent(block, content, close, blur) {
     block.addEventListener("click", function() {
         content.classList.add("show-content")
+        blur.classList.add("blur-active")
     })
     close.addEventListener("click", function() {
         content.classList.remove("show-content")
+        blur.classList.remove("blur-active")
     })
 }
 
 const block = document.querySelectorAll(".block")
 const contents = document.querySelectorAll(".content")
 const closes = document.querySelectorAll(".close")
+const blur = document.querySelector(".blur")
 
 block.forEach((block, index) => {
-    toggleContent(block, contents[index], closes[index])
+    toggleContent(block, contents[index], closes[index], blur)
 })
